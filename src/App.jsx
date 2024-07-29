a2a463c5f8ee8b31ce0509186f337d4e8b12aa5b
missmurs <131698859+missmurs@users.noreply.github.com>
2024-07-29 20:13:13 +0300

    1

--- src/App.jsx
+++ src/App.jsx
@@ -6,10 +6,8 @@ import Feedback from "./componets/Feedback/Feedback";
 import Options from "./componets/Options/Options";
 import Notification from "./componets/Notification/Notification";
 function App() {
-  const [feedback, setFeedback] = useState({
-    good: 0,
-    neutral: 0,
-    bad: 0,
+  const [feedback, setFeedback] = useState(() => {
+    return JSON.parse(localStorage.getItem("saved-feedback"));
   });
   const updateFeedback = (feedbackType) => {
     setFeedback({ ...feedback, [feedbackType]: feedback[feedbackType] + 1 });
